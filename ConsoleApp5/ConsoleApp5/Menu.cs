112ac67901b2d642d92a0fd29bd40c8370b18603
DiamondD7 <97176132+DiamondD7@users.noreply.github.com>
2022-03-22 02:18:17 +1300

    Add files via upload
    This is only a demo repo

---
+++ ConsoleApp5/ConsoleApp5/Menu.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Menu
+    {
+        public string FoodName { get; set; }
+        public int FoodId { get; set; }
+        public double FoodPrice { get; set; }
+        public double FinalPrice
+        {
+            get { return FoodPrice + Gst; }
+        }
+        public double Gst
+        {
+            get { return (15 / 100) * FoodPrice; }
+        }
+
+        public Menu(string _foodname, int _foodid, double _foodprice)
+        {
+            FoodName = _foodname;
+            FoodId = _foodid;
+            FoodPrice = _foodprice;
+        }
+   
+    }
+}
